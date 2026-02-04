160d0cd0e9ca9baf88035e54a6f0ad09c5cc0a34
jnasbyupgrade <jnasby@upgrade.com>
2026-02-04 17:47:11 -0600

    Update to latest pgxntool standards
    Minor tweaks to test setup. Also, start keeping versioned extension files in git.

--- test/load.sql
+++ test/load.sql
@@ -1,15 +1,2 @@
+-- Pulls in test/deps.sql as well
 \i test/pgxntool/setup.sql
-
-SET search_path = tap, public;
-
--- Don't use IF NOT EXISTS here; we want to ensure we always have the latest code
-SET client_min_messages = WARNING; -- Squelch notices about dependent extensions
-
-CREATE SCHEMA IF NOT EXISTS :schema;
-SET search_path = :schema;
-
-CREATE EXTENSION object_reference CASCADE;
-
-CREATE EXTENSION count_nulls;
-
---SET client_min_messages = NOTICE;
