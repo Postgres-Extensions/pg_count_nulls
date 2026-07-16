7fa7666a9c9a92345e007c770e15d465512ee952
jnasbyupgrade <jnasby@upgrade.com>
2026-07-16 13:30:59 -0500

    Switch to community pgTap (#8)
    - Drop the unsupported `--load-language=plpgsql` flag on PG13+. Temporary hack until we update to a newer pgxntool
    - Customize `test/deps.sql` to create the `count_nulls` extension (it
    was previously just a comment stub)
    - Remove pgTap from the repository (depending on external install instead)

--- test/deps.sql
+++ test/deps.sql
@@ -1,3 +1,10 @@
 -- Note: pgTap is loaded by setup.sql
 
 -- Add any test dependency statements here
+-- IF NOT EXISTS will emit NOTICEs, which is annoying
+SET client_min_messages = WARNING;
+CREATE SCHEMA IF NOT EXISTS :schema;
+SET search_path = :schema;
+SET client_min_messages = NOTICE;
+
+CREATE EXTENSION count_nulls;
